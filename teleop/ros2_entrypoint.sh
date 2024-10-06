8c62c00e260d2923806c34205f9ceac640523bf4
Cecilia Liu <ceciliaxiaoxiliu@gmail.com>
2024-10-05 21:48:26 -0400

    adds sourcing of ros2 workspace in teleop container at entry to use rosimagesrc

--- teleop/ros2_entrypoint.sh
+++ teleop/ros2_entrypoint.sh
@@ -4,6 +4,8 @@
 source /opt/ros/humble/setup.bash
 source /opt/ros/humble/share/ros_environment/local_setup.bash
 
+# source ros2 workspace used for installing rosimagesrc
+source /opt/app/ros2_ws/install/setup.bash
 
 # Start the rosbridge server
 # ros2 launch rosbridge_server rosbridge_websocket_launch.xml
